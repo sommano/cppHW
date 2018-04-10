6e40ae444e1c1db6485b8ea548cbecb488b59c9b
sommano phivilay <sommano@gmail.com>
2018-04-10 17:30:21 -0400

    cha 3

---
+++ Var/var.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int variable;
+int main()
+{
+    variable = 42;
+
+    cout << variable;
+    return 0;
+}
---
+++ operators/operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int sum = 0;
+int main() {
+    sum = 64 + 32;
+    cout << sum;
+
+    return 0;
+}
